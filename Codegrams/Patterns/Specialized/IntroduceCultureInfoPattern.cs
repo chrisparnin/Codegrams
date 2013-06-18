96335c084c68693710b02ecf83dbb3655d73cc90
Chris Parnin <chris.parnin@gmail.com>
2013-06-18 04:39:02 -0400

    Add file to csproj.

---
+++ Codegrams/Patterns/Specialized/IntroduceCultureInfoPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codegrams.Patterns.Specialized
+{
+    class IntroduceCultureInfoPattern : Pattern
+    {
+        public override Grams.Codegram Rewrite(Grams.Codegram gram)
+        {
+            foreach (var file in gram.Filegrams)
+            {
+                foreach (var line in file.Linegrams)
+                {
+                    // Extract inner line differences?
+                    // "-"
+                    // "+", CultureInfo.CurrentCulture
+                }
+            }
+            return gram;
+        }
+    }
+}
